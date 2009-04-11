1d04c1161b6c38ae3f3edb3852103fef771a7c49
rif <rif@grace>
2009-04-11 23:44:11 +0300

    spatii

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 
 class MatchDay(models.Model):
-	matchdate_date = models.DateTimeField()
-	location = models.CharField(max_length=50)
+    matchdate_date = models.DateTimeField()
+    location = models.CharField(max_length=50)
 
-	def __unicode__(self):
-		return self.matchdate_date.strftime('%d-%B-%Y')\
-		+ ' ' + self.location\
-		+ ' ' + self.matchdate_date.strftime('%H:%M')
-
-
+    def __unicode__(self):
+        return self.matchdate_date.strftime('%d-%B-%Y')\
+        + ' ' + self.location\
+        + ' ' + self.matchdate_date.strftime('%H:%M')
 
 class Player(models.Model):
-	attended_matches = models.ManyToManyField(MatchDay)
-	first_name = models.CharField(max_length=50)
-	last_name = models.CharField(max_length=50)
-	alias_name = models.CharField(max_length=50)
+    attended_matches = models.ManyToManyField(MatchDay)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    alias_name = models.CharField(max_length=50)
 
-	def __unicode__(self):
-		return self.first_name + ' ' + self.alias_name + ' ' + self.last_name
+    def __unicode__(self):
+        return self.first_name + ' ' + self.alias_name + ' ' + self.last_name
