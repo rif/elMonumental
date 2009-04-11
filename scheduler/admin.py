166096448bf8d21ccb979b7e19ba892cbd71b89d
rif <rif@grace>
2009-04-11 23:41:54 +0300

    am mai lucrat la modele

---
+++ scheduler/admin.py
@@ -0,0 +1,5 @@
+from elMonumental.scheduler.models import MatchDay, Player
+from django.contrib import admin
+
+admin.site.register(MatchDay)
+admin.site.register(Player)
--- scheduler/models.py
+++ scheduler/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
-class MatchDay(models.Model);
-	pass
+class MatchDay(models.Model):
+	matchdate_date = models.DateTimeField()
+	location = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return self.matchdate_date.strftime('%d-%B-%Y')\
+		+ ' ' + self.location\
+		+ ' ' + self.matchdate_date.strftime('%H:%M')
+
+
 
 class Player(models.Model):
-	pass
+	attended_matches = models.ManyToManyField(MatchDay)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	alias_name = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return self.first_name + ' ' + self.alias_name + ' ' + self.last_name
