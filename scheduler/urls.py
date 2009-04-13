a9f237bdbf25206f5ecf525a5bf01aa377d86bf4
Radu Fericean <radu.fericean@oce.com>
2009-04-13 15:32:33 +0300

    am inceput sa fac templates si viewes

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'elMonumental.scheduler.views.index'),
+    (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'scheduler/login.html'}),
 )
--- settings.py
+++ settings.py
@@ -50,9 +50,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'elMonumental.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    '/home/rif/1/prog/webframeworks/elMonumental/templates/',
 )
 
 INSTALLED_APPS = (
