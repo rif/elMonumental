da13ba904af0735ffe1a824fd14e489e936efbf6
Radu Ioan Fericean <radu@fericean.ro>
2009-04-13 15:32:33 +0300

    am inceput sa fac templates si viewes

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("Hello world!")
+    return render_to_response('scheduler/index.html', {})
+
