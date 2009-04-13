9764e86f455f05e256f21e5357c319b874aa1934
Radu Ioan Fericean <radu@fericean.ro>
2009-04-13 11:59:16 +0300

    am facut modelul, ma pot apuca de lucru

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
-class MatchDay(models.Model):
-    matchdate_date = models.DateTimeField()
-    location = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.matchdate_date.strftime('%d-%B-%Y')\
-        + ' ' + self.location\
-        + ' ' + self.matchdate_date.strftime('%H:%M')
-
-class Player(models.Model):
-    attended_matches = models.ManyToManyField(MatchDay)
+class Player(models.Model):    
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     alias_name = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.first_name + ' ' + self.alias_name + ' ' + self.last_name
+
+class MatchDay(models.Model):
+    matchdate_date = models.DateTimeField()
+    location = models.CharField(max_length=50)
+    attendee_players = models.ManyToManyField(Player)
+
+    def __unicode__(self):
+        return self.matchdate_date.strftime('%d-%B-%Y')\
+        + ' ' + self.location\
+        + ' ' + self.matchdate_date.strftime('%H:%M')
