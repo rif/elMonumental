a801f4460c2882545c31a977ee0154299f1cf0ab
Radu Ioan Fericean <radu@fericean.ro>
2009-04-14 16:56:34 +0300

    lucrez la attend

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
-def index(request):
-    return render_to_response('scheduler/index.html', {})
+@login_required
+def attend(request, object_id):
+    return render_to_response('scheduler/attend.html', {})
 
