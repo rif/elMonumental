e551763e4cbd9673af8a518d5233f68488b960c2
Radu Fericean <radu.fericean@oce.com>
2009-04-14 18:05:40 +0300

    nu stiu de ce nu imi ia session data in unele viewuri

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^$', 'django.views.generic.list_detail.object_list', info, name='matchday-list'),
     url(r'^attend/(?P<object_id>\d+)/$', 'scheduler.views.attend', name='matchday-attend'),
     url(r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info, name='matchday-detail'),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'scheduler/login.html', 'next':'/'}, name='login-link'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'scheduler/login.html'}, name='login-link'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'scheduler/login.html', 'next_page':'/'}, name='logout-link'),
 )
