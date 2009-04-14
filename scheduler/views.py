b332febcac2ba0518209b01a924855c9d1ffa8db
Radu Ioan Fericean <radu@fericean.ro>
2009-04-14 18:05:40 +0300

    nu stiu de ce nu imi ia session data in unele viewuri

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from models import MatchDay
 
 @login_required
 def attend(request, object_id):
-    return render_to_response('scheduler/attend.html', {})
+    md = get_object_or_404(MatchDay, pk=object_id)
+    return render_to_response('scheduler/attend.html', {'matchday':md})
 
