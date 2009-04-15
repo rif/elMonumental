847002a6938a5274ff21971cd727a7822946a986
Radu Fericean <radu.fericean@oce.com>
2009-04-15 12:01:12 +0300

    am modificat sa fie playerii extensie la usersi, il las in stare ne-utilizabila

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -2,4 +2,4 @@ from elMonumental.scheduler.models import MatchDay, Player
 from django.contrib import admin
 
 admin.site.register(MatchDay)
-admin.site.register(Player)
+admin.site.register(Team)
--- scheduler/models.py
+++ scheduler/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
-class Player(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
     alias_name = models.CharField(max_length=50)
-    team = models.CharField(max_length=50, blank=True)
-
-    def __unicode__(self):
-        return self.first_name + ' ' + self.alias_name + ' ' + self.last_name
+    goals_scored = models.IntegerField(blank=True)
+    speed = models.IntegerField(blank=True)
+    stamina = models.IntegerField(blank=True)
+    ball_controll = models.IntegerField(blank=True)
+    shot_power = models.IntegerField(blank=True)
+    transfer_sum = models.IntegerField(blank=True)
 
 class MatchDay(models.Model):
     start_date = models.DateTimeField()
     location = models.CharField(max_length=50)
-    attendee_players = models.ManyToManyField(Player)
+    attendee_players = models.ManyToManyField(User, through='Team')
     scoreA = models.IntegerField(blank=True)
     scoreB = models.IntegerField(blank=True)
 
@@ -24,3 +26,8 @@ class MatchDay(models.Model):
 
     def  isFuture(self):
         return datetime.today() < self.start_date
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    matchDay = models.ForeignKey(MatchDay)
+    members = models.ForeignKey(User)
