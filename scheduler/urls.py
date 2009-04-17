8ad624d8756c18671e342f67a418abb83f9c8e1b
Radu Ioan Fericean <radu@fericean.ro>
2009-04-18 01:01:27 +0300

    am inceput crearea de useri

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -11,7 +11,8 @@ urlpatterns = patterns('',
     url(r'^attend/(?P<object_id>\d+)/$', 'scheduler.views.attend', name='matchday-attend'),
     url(r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info, name='matchday-detail'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'scheduler/login.html'}, name='login-link'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'scheduler/login.html', 'next_page':'/'},
-    name='logout-link'),
+    url(r'^accounts/signup/$', 'scheduler.views.signup', name='signup-link'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'scheduler/login.html', 'next_page':'/'}, name='logout-link'),
     (r'^accounts/profile/', 'django.views.generic.list_detail.object_list', info),
+
 )
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import UserManager
 from models import MatchDay
 
 @login_required
@@ -9,4 +11,17 @@ def attend(request, object_id):
     md.participants.add(request.user)
     return render_to_response('scheduler/attend.html',
                               {'matchday':md},
+                              context_instance=RequestContext(request))
+
+def signup(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = UserCreationForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            user = UserManager.create_user(form.username, 'test@oce.com' ,form.password1)
+            user.message_set.create(message="Your user was created succesfuly.")
+            return HttpResponseRedirect('/') # Redirect after POST
+    else:
+        form = UserCreationForm() # An unbound form
+    return render_to_response('scheduler/signup.html',
+                              {'form': form,},
                               context_instance=RequestContext(request))
