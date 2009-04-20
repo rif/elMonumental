3f762675667ad4d1ebb553510c5f0a4b6d34f966
Radu Ioan Fericean <radu@fericean.ro>
2009-04-20 21:07:07 +0300

    am facut destul de mult la player profile dar nu ii inca gata

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -14,6 +14,4 @@ urlpatterns = patterns('',
     url(r'^accounts/signup/$', 'scheduler.views.signup', name='signup-link'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'scheduler/login.html', 'next_page':'/'}, name='logout-link'),
     url(r'^accounts/profile/$', 'scheduler.views.profile', name='profile-link'),
-    (r'^accounts/profile/', 'django.views.generic.list_detail.object_list', info),
-
 )
--- scheduler/views.py
+++ scheduler/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
-from django.contrib.auth import authenticate
-from models import MatchDay, UserProfileForm
+from django.contrib.auth import authenticate, login
+from models import MatchDay, PlayerProfileForm, PlayerProfile
 
 @login_required
 def attend(request, object_id):
@@ -19,9 +19,9 @@ def signup(request):
         form = UserCreationForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             newUser = form.save()
-            user = authenticate(username=newUser.username, password=newUser.password)
-            #user.message_set.create(message = "%s have registered succesfuly." % newUser.username)
-            return HttpResponseRedirect('/accounts/profile/') # Redirect after POST
+            user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password1'])
+            login(request, user)
+            return HttpResponseRedirect('/accounts/profile/%s' % user.id) # Redirect after POST
     else:
         form = UserCreationForm() # An unbound form
     return render_to_response('scheduler/signup.html',
@@ -30,15 +30,42 @@ def signup(request):
 
 def profile(request):
     if request.method == 'POST': # If the form has been submitted...
-        form = UserProfileForm(request.POST) # A form bound to the POST data
+        form = PlayerProfileForm(request.POST) # A form bound to the POST data
         form.user = request.user
         if form.is_valid(): # All validation rules pass
             user = request.user
-            user.get_profile().alias_name = form.alias_name
+            profile = user.get_profile()
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.email = form.cleaned_data['email']
+            profile.alias_name = form.cleaned_data['alias_name']
+            profile.goals_scored = form.cleaned_data['goals_scored']
+            profile.speed = form.cleaned_data['speed']
+            profile.stamina = form.cleaned_data['stamina']
+            profile.ball_controll = form.cleaned_data['ball_controll']
+            profile.shot_power = form.cleaned_data['shot_power']
+            profile.transfer_sum = form.cleaned_data['transfer_sum']
             user.save()
+            profile.save()
             return HttpResponseRedirect('/') # Redirect after POST
     else:
-        form = UserProfileForm() # An unbound form
+        try:
+            pp = request.user.get_profile()
+        except PlayerProfile.DoesNotExist:
+            pp = PlayerProfile(user = request.user)
+            pp.save()
+        data = {'first_name': request.user.first_name,
+                'last_name': request.user.last_name,
+                'email': request.user.email,
+                'alias_name': pp.alias_name,
+                'goals_scored': pp.goals_scored,
+                'speed': pp.speed,
+                'stamina': pp.stamina,
+                'ball_controll': pp.ball_controll,
+                'shot_power': pp.shot_power,
+                'transfer_sum': pp.transfer_sum,
+                }
+        form = PlayerProfileForm(data) # An unbound form
     return render_to_response('scheduler/profile.html',
                               {'form': form,},
                               context_instance=RequestContext(request))
