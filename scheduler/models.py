2f42ba502a3f498e14ef6ff3b6dbd0765b956a2c
rif <rif@grace>
2009-04-20 21:07:07 +0300

    am facut destul de mult la player profile dar nu ii inca gata

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,26 +1,33 @@
 from django.db import models
 from datetime import datetime
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django import forms
 
 SPEED_CHOICES = (('SN', 'Snail'), ('PD', 'Pedestrian'), ('SP', 'Sprinter'), ('RK', 'Rocket'),)
 STAMINA_CHOICES = (('SL', 'Sleep Walker'), ('PR', 'Programmer'), ('PD', 'Paladin LV7'), ('MR', 'Marathonist'),)
 CONTROLL_CHOICES = (('LP', 'Light Post'), ('EV', 'Evitationist'), ('NW', 'Needle Worker'), ('RN', 'Ronaldinho'),)
 SHOT_CHOICES = (('SN', 'Snail'), ('KK', 'Kicker'), ('GD', 'Gigi Duru'), ('GN', 'Gunner'),)
 
+
 class PlayerProfile(models.Model):
     user = models.ForeignKey(User, null=True, unique=True)
     alias_name = models.CharField(max_length=50)
     goals_scored = models.IntegerField(null=True,blank=True)
-    speed = models.CharField(max_length=3, choices=SPEED_CHOICES)
-    stamina = models.CharField(max_length=3, choices=STAMINA_CHOICES)
-    ball_controll = models.CharField(max_length=3, choices=CONTROLL_CHOICES)
-    shot_power = models.CharField(max_length=3, choices=SHOT_CHOICES)
+    speed = models.CharField(null=True,blank=True, max_length=3, choices=SPEED_CHOICES)
+    stamina = models.CharField(null=True,blank=True, max_length=3, choices=STAMINA_CHOICES)
+    ball_controll = models.CharField(null=True,blank=True, max_length=3, choices=CONTROLL_CHOICES)
+    shot_power = models.CharField(null=True,blank=True, max_length=3, choices=SHOT_CHOICES)
     transfer_sum = models.IntegerField(null=True, blank=True)
 
-class UserProfileForm(ModelForm):
+class PlayerProfileForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
     class Meta:
         model = PlayerProfile
+        exclude = ('user',)
+
 
 class MatchDay(models.Model):
     start_date = models.DateTimeField()
@@ -38,3 +45,10 @@ class MatchDay(models.Model):
 
     class Meta:
         ordering = ["-start_date"]
+
+def user_profile_handler(sender, **kwargs):
+    if kwargs['created'] == True:
+        pp = PlayerProfile(user=kwargs['instance'])
+        pp.save()
+
+post_save.connect(user_profile_handler, sender=User)
