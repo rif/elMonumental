18d3070150fcfa70df99a3c219254cc3b46e3862
Radu Fericean <radu.fericean@oce.com>
2009-04-27 12:44:15 +0300

    am mai pus o cheste de user detail si trebuie sa fac guest stars adding/removing

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list, object_detail
 from scheduler.views import *
+from django.contrib.auth.models import User
 from models import MatchDay
 
 info = {
     'queryset': MatchDay.objects.all(),
+}
+
+users = {
+    'queryset': User.objects.all(),
+    'template_name': 'scheduler/user_detail.html'
 }
 
 urlpatterns = patterns('django.contrib.auth.views',
@@ -12,9 +18,10 @@ urlpatterns = patterns('django.contrib.auth.views',
     url(r'^attend/(?P<object_id>\d+)/$', attend, name='matchday-attend'),
     url(r'^abandon/(?P<object_id>\d+)/$', abandon, name='matchday-abandon'),
     url(r'^matchday/(?P<object_id>\d+)/$', object_detail, info, name='matchday-detail'),
+    url(r'^profile/(?P<object_id>\d+)/$', object_detail, users, name='profile-link'),
     url(r'^accounts/login/$', 'login', {'template_name': 'scheduler/login.html'}, name='login-link'),
     url(r'^accounts/signup/$', signup, name='signup-link'),
     url(r'^accounts/logout/$', 'logout', {'template_name': 'scheduler/login.html', 'next_page':'/'}, name='logout-link'),
-    url(r'^accounts/profile/$', profile, name='profile-link'),
+    url(r'^accounts/profile/$', profile, name='profile-edit-link'),
     url(r'^links/(?P<md_id>\d{1})/$', linkQuerry, name='request-callback'),
 )
