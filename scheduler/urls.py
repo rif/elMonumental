e44d61e54a2be9629faee6c4966c57c5d93c2d75
Radu Ioan Fericean <radu@fericean.ro>
2009-04-29 12:54:46 +0300

    o sa mai continui alta data

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,20 +4,20 @@ from scheduler.views import *
 from django.contrib.auth.models import User
 from models import MatchDay
 
-info = {
+md_info = {
     'queryset': MatchDay.objects.all(),
 }
 
 user_info = {
     'queryset': User.objects.all(),
-    'template_name': 'scheduler/user_detail.html'
+    'template_name': 'scheduler/user_detail.html',
 }
 
 urlpatterns = patterns('django.contrib.auth.views',
-    url(r'^$', object_list, info, name='matchday-list'),
+    url(r'^$', object_list, md_info, name='matchday-list'),
     url(r'^attend/(?P<md_id>\d+)/$', attend, name='matchday-attend'),
     url(r'^abandon/(?P<md_id>\d+)/$', abandon, name='matchday-abandon'),
-    url(r'^matchday/(?P<object_id>\d+)/$', object_detail, info, name='matchday-detail'),
+    url(r'^matchday/(?P<object_id>\d+)/$', object_detail, md_info, name='matchday-detail'),
     url(r'^profile/(?P<object_id>\d+)/$', object_detail, user_info, name='profile-link'),
     url(r'^accounts/login/$', 'login', {'template_name': 'scheduler/login.html'}, name='login-link'),
     url(r'^accounts/signup/$', signup, name='signup-link'),
