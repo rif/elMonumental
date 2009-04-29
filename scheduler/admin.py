fc1ddea729ce4aa3091649f5a329f177591c12b3
Radu Ioan Fericean <radu@fericean.ro>
2009-04-29 12:38:59 +0300

    am mai remediat din probleme dar nu toate

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,5 +1,6 @@
-from elMonumental.scheduler.models import MatchDay, PlayerProfile
+from elMonumental.scheduler.models import MatchDay, PlayerProfile, GuestPlayer
 from django.contrib import admin
 
 admin.site.register(MatchDay)
 admin.site.register(PlayerProfile)
+admin.site.register(GuestPlayer)
