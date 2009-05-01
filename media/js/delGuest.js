fae46d98672b9b0f209f624cbef8a248c0aa2e4f
rif <rif@grace>
2009-05-01 21:47:27 +0300

    merge delete de guests

---
+++ media/js/delGuest.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("a.dellink").click(function(e){
+        $.post("/links/delguest/",
+            { md_id: $(this).attr('matchdayId'),
+              guest_id: $(this).attr('guestId'),
+            });
+        $(this).replaceWith('Deleted');
+    });
+});
