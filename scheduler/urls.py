20c65ddfecf701e10158cfdbab7978b12ad6147d
Radu Fericean <radu.fericean@oce.com>
2009-06-02 16:28:09 +0300

    cred ca merge registerul

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -6,13 +6,7 @@ md_info = {
     'queryset': MatchDay.objects.all(),
 }
 
-
-urlpatterns = patterns('django.contrib.auth.views',
-    #url(r'^accounts/login/$', 'login', {'template_name': 'scheduler/login.html'}, name='sch_login'),
-    #url(r'^accounts/logout/$', 'logout', {'template_name': 'scheduler/login.html', 'next_page':'/'}, name='sch_logout'),
-)
-
-urlpatterns += patterns('django.views.generic.list_detail',
+urlpatterns = patterns('django.views.generic.list_detail',
     url(r'^$', 'object_list', md_info, name='sch_matchday-list'),
     url(r'^matchday/(?P<object_id>\d+)/$', 'object_detail', md_info, name='sch_matchday-detail'),
 )
@@ -21,7 +15,6 @@ urlpatterns += patterns('',
     url(r'^attend/(?P<md_id>\d+)/$', views.attend, name='sch_matchday-attend'),
     url(r'^abandon/(?P<md_id>\d+)/$', views.abandon, name='sch_matchday-abandon'),
     #url(r'^accounts/profile/$', views.profileInfo, name='sch_profile'),
-    #url(r'^accounts/signup/$', views.signup, name='sch_signup'),
     url(r'^profileedit/$', views.profile, name='sch_profile-edit'),
     url(r'^links/$', views.linkQuerry, name='sch_request-ajax'),
     url(r'^addguest/(?P<md_id>\d+)/$', views.addGuest, name='sch_addguest'),
