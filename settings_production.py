57d5b0907a2fe6cc1115a4716bf4aa967d0bfd59
Radu Ioan Fericean <radu@fericean.ro>
2009-06-04 11:26:05 +0300

    setari de productie

--- settings_production.py
+++ settings_production.py
@@ -1,5 +1,10 @@
 from settings import *
 
 DEBUG = TEMPLATE_DEBUG = False
-DATABASE_NAME = rel('production.sqlite')
-
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'elMonumental'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'coadasoricelului'
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+MEDIA_URL = 'http://10.40.8.206/media/'
