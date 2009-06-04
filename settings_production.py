a6d1134894629776ba802897650e64bfa1ce006d
Radu Ioan Fericean <radu@fericean.ro>
2009-06-04 11:42:12 +0300

    am schimbat userul de bd

--- settings_production.py
+++ settings_production.py
@@ -3,8 +3,8 @@ from settings import *
 DEBUG = TEMPLATE_DEBUG = False
 DATABASE_ENGINE = 'mysql'
 DATABASE_NAME = 'elMonumental'
-DATABASE_USER = 'root'
-DATABASE_PASSWORD = 'coadasoricelului'
+DATABASE_USER = 'elAdmin'
+DATABASE_PASSWORD = 'testus_cumulus'
 DATABASE_HOST = ''
 DATABASE_PORT = ''
 MEDIA_URL = 'http://10.40.8.206/media/'
