bbec2f12bd80c80b180829dd57dc7ffdd3b1c494
Radu Ioan Fericean <radu@fericean.ro>
2009-06-05 13:44:37 +0300

    am mai adaugat laptopul la development servers

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import socket
-if socket.gethostname() == 'sro-9090.sro.oce.net':
+if socket.gethostname() in ('sro-9090.sro.oce.net', 'grace'):
     from settings_development import *
 else:
     from settings_production import *
