745453668b32152b7f77e312d1a3b336376a2f89
rif <rif@love>
2009-07-05 14:58:22 +0300

    am adaugat comments

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import socket
-if socket.gethostname() in ('sro-9090.sro.oce.net', 'grace'):
+if socket.gethostname() in ('sro-9090.sro.oce.net', 'grace', 'love'):
     from settings_development import *
 else:
     from settings_production import *
--- urls.py
+++ urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('registration.urls')),
     (r'^profiles/', include('profiles.urls')),
+    (r'^comments/', include('django.contrib.comments.urls')),
 )
 # if we're in DEBUG mode, allow django to serve media
 # This is considered inefficient and isn't secure.
