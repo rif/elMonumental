0cb8288eb4f65f400e375876f15be4176a7b2423
rif <rif@love>
2009-08-15 15:21:06 +0300

    more test

--- news/tests.py
+++ news/tests.py
@@ -2,13 +2,24 @@ from django.test import TestCase
 from news.models import News
 
 class NewsTest(TestCase):
+    def setUp(self):
+        self.news = News.objects.create(title="Mama are mere", slug="mama-are-mere", content="Test de continut")
+
     def test_basic_addition(self):
-        news = News(title="Mama are mere", content="Test de continut")
-        self.failUnlessEqual(str(news), "Mama are mere")
+        self.failUnlessEqual(str(self.news), "Mama are mere")
 
     def test_feed_content(self):
         response = self.client.get('/feeds/news/')
         self.failUnlessEqual(response.status_code, 200)
-        self.failUnlessEqual(response.content.count('<title>'), 1)
-        self.failUnlessEqual(response.content.count('<description>'), 1)
+        self.failUnlessEqual(response.content.count('<title>'), 2)
+        self.failUnlessEqual(response.content.count('<description>'), 2)
 
+    def test_list(self):
+        response = self.client.get('/news/')
+        self.failUnlessEqual(response.status_code, 200)
+        self.failUnlessEqual(response.content.count('<li>'), 1)
+
+    def test_detail(self):
+        response = self.client.get('/news/mama-are-mere/')
+        self.failUnlessEqual(response.status_code, 200)
+        self.failUnlessEqual(response.content, '\n\n<h1>Mama are mere</h1>\n<p>Sat 15 Aug 2009 -\n  15:20</p>\n\n\n')
