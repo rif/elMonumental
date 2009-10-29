ba53da66f6fed35116bb260374759a6b037f3d47
Radu Fericean <radu.fericean@oce.com>
2009-10-29 10:26:38 +0200

    renunt la cache

--- news/urls.py
+++ news/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls.defaults import *
 from news import views
 from news.models import News
-from django.views.decorators.cache import cache_page
 
 latest_news = {'queryset': News.objects.all()[:5]}
 all_news = {'queryset': News.objects.all()}
 
 urlpatterns = patterns('django.views.generic.list_detail',
-    url(r'^$', 'object_list',
-        cache_page(latest_news),
+    url(r'^$',
+        'object_list',
+        latest_news,
         name='news_news_list'),
     url(r'^(?P<slug>[-\w]+)/$',
         'object_detail',
-        cache_page(all_news),
+        all_news,
         name='news_news_detail'),
     url(r'^(?P<slug>[-\w]+)/rss/$',
         'object_detail',
