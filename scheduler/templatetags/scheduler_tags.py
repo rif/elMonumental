e93ee01dab29bcc90d1f5a3af5075a2fb7491412
Radu Ioan Fericean <fericean@gmail.com>
2010-02-01 21:39:47 +0200

    io stiu ce am mai facut

--- scheduler/templatetags/scheduler_tags.py
+++ scheduler/templatetags/scheduler_tags.py
@@ -7,17 +7,17 @@ def do_atendance(parser, token):
     return AtendanceNode()
 
 class AtendanceNode(template.Node):
-    def render(self, context):        
-        username = context['profile'].user.username        
+    def render(self, context):
+        username = context['profile'].user.username
         for sport in (MatchDay.FOOTBALL, MatchDay.VOLLEYBALL, MatchDay.BASKETBALL):
             mds = MatchDay.objects.filter(sport=sport)
             """if context['filter_date'] != None:
                 mds = mds.filter(start_date__gte=datetime(2009, 7, 29))"""
             mdsCount = mds.count()
-            count = mds.filter(participants__username=username).count()                       
+            count = mds.filter(participants__username=username).count()
             procentage = "NA"
             if mdsCount > 0:
-                procentage = str(count*100/mdsCount) + "%"         
+                procentage = str(count*100/mdsCount) + "%"
             context[sport + '_count'] = str(count) + "/" + str(mdsCount)
             context[sport + '_procentage'] = procentage
         return ''
