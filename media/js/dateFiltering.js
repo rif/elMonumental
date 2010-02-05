3943455e0b6a61100485f4e0758a56d1b32a2080
Radu Ioan Fericean <radu@fericean.ro>
2010-02-05 21:26:11 +0200

    sper ca se filtreaza lista acum

--- media/js/dateFiltering.js
+++ media/js/dateFiltering.js
@@ -1,11 +1,15 @@
 $(function() {
-    $("#since").datepicker({dateFormat: 'dd-mm-yy',
-                                                             onClose: function(dateText, inst) { filtrare(); }
-			                                            });
-      $("a#filter").click(function(e){
-			      filtrare();
-			      e.preventDefault();
-			  });
+    $("#since").datepicker({
+                dateFormat: 'dd-mm-yy',
+                onClose: function(dateText, inst) {
+                            filtrare();
+                }
+    });
+    $("a#filter").click(function(e){
+                filtrare();
+                e.preventDefault();
+    });
+    sortTable();
   });
 
 function filtrare(){
@@ -13,5 +17,14 @@ function filtrare(){
                     function(responseData){
             	       $("tbody").html(responseData);
 	   });
+    sortTable();
 }
 
+function sortTable(){
+     $("#usersTable").tablesorter(
+                    {headers: { 8: { sorter: false},
+                                10: {sorter: false},
+                                12: {sorter: false} }
+                    }
+     );
+}
