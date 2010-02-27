0df49d223369d7a0d1a49aff2e96e7cece39d36b
Radu Ioan Fericean <radu@fericean.ro>
2010-02-27 17:01:14 +0200

    get_sport_display inlocuit cu sport_name

--- scheduler/feeds.py
+++ scheduler/feeds.py
@@ -1,26 +1,27 @@
 from django.contrib.syndication.feeds import Feed
 from scheduler.models import MatchDay
 from news.models import News
+from scheduler.models import Sport
 
 class LatestMatchDays(Feed):
     title = "elMonumental"
     link = '/'
-    description = "Updates on changes and additions to elMonumental."
+    description = "elMonumental matchdays additions."
 
     def items(self):
         return MatchDay.objects.order_by('-start_date')[:5]
 
     def item_link(self, item):
         return '/matchday/' + str(item.id) + '/rss/'
-    
+
     def item_categories(self, item):
-        return [c[1] for c in item.SPORT_CHOICES]
+        return [sport.name for sport in Sport.objects.all()]
 
 class LatestNews(Feed):
     title = "elMonumental news"
     link = "/news/"
     description = "Latest news about football and elMonumental."
-    
+
     def items(self):
         return News.objects.order_by('-pub_date')[:5]
 
