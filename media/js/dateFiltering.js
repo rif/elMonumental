e28436cae63c63631e9907ba0a824ecc1d57c22e
Radu Ioan Fericean <rif@grace.ro>
2010-03-02 18:30:16 +0200

    am implementat filtrarea cu POST si am schimbat tema la calendar

--- media/js/dateFiltering.js
+++ media/js/dateFiltering.js
@@ -2,31 +2,20 @@ $(function() {
     $("#since").datepicker({
                 dateFormat: 'dd-mm-yy',
                 onClose: function(dateText, inst) {
-                            filtrare();
                 }
-    });
-    $("a#reset").click(function(e){
-                $("#since").val('');
-                filtrare();
-                e.preventDefault();
     });
     sortTable();
   });
-
-function filtrare(){
-    $.post('/filterProfiles/', {'since': $("#since").val()},
-                    function(responseData){
-            	       $("tbody").html(responseData);
-	   });
-    sortTable();
-}
 
 function sortTable(){
      $("#usersTable").tablesorter(
                     {headers: { 8:  {sorter: false},
                                 10: {sorter: false},
-                                12: {sorter: false} }
+                                12: {sorter: false}, 
+				14: {sorter: false},
+				},
+		    widthFixed: true
                     }
-     );
+     );//.tablesorterPager({container: $("#pager")});
 }
 
