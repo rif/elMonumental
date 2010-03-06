91527ede7ee4c980d69f9010f448aa259d3d743e
Radu Ioan Fericean <radu@fericean.ro>
2010-03-06 20:33:20 +0200

    small changes maybe optimisations

--- scheduler/templatetags/scheduler_tags.py
+++ scheduler/templatetags/scheduler_tags.py
@@ -2,26 +2,30 @@ from django import template
 from scheduler.models import MatchDay, Sport
 import logging
 
-def do_atendance(parser, token):
+register = template.Library()
+
+@register.tag('get_attendance')
+def do_attendance(parser, token):
     return AtendanceNode()
 
+@register.tag('query_matchdays')
 def do_query_matchdays(parser, token):
     return MatchdayQueryNode()
 
 class MatchdayQueryNode(template.Node):
     def render(self, context):
-        for sport in Sport.objects.iterator():
+        for sport in context['sport_list']:
             mds = MatchDay.objects.filter(sport_name=sport)
             context[sport.name + '_query'] = mds
             context[sport.name + '_counter'] = mds.count()
         return ''
 
-
+""" Depends on MatchdayQueryNode being executed for efficiency """
 class AtendanceNode(template.Node):
     def render(self, context):
         username = context['profile'].user.username
         result_dict = {}
-        for sport in Sport.objects.iterator():
+        for sport in context['sport_list']:
             mds = context[sport.name + '_query']
             mdsCount = context[sport.name + '_counter']
             if context.has_key('since'):
@@ -34,11 +38,6 @@ class AtendanceNode(template.Node):
 #            context[sport.name + '_count'] = str(count) + "/" + str(mdsCount)
 #            context[sport.name + '_procentage'] = procentage
             result_dict[sport.name] = (str(count) + "/" + str(mdsCount), procentage)
-        context['atendance_dict'] = result_dict
-        logging.debug(result_dict)
+        context['attendance_dict'] = result_dict
         return ''
-
-register = template.Library()
-register.tag('get_atendance', do_atendance)
-register.tag('query_matchdays', do_query_matchdays)
 
