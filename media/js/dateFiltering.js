dd1509d6557f074267067fe94e141be2633a26d6
Radu Ioan Fericean <rif@grace.ro>
2010-03-07 21:46:31 +0200

    am incercat sa pun sorting false pentru viitoare sporturi

--- media/js/dateFiltering.js
+++ media/js/dateFiltering.js
@@ -11,10 +11,16 @@ function sortTable(){
      $("#usersTable").tablesorter(
                     {headers: { 8:  {sorter: false},
                                 10: {sorter: false},
-                                12: {sorter: false}, 
-				14: {sorter: false},
-				},
-		    widthFixed: true
+                                12: {sorter: false},
+                                14: {sorter: false},
+                                16: {sorter: false},
+                                18: {sorter: false},
+                                20: {sorter: false},
+                                22: {sorter: false},
+                                24: {sorter: false},
+                                26: {sorter: false},
+                               },
+                     widthFixed: true
                     }
      );//.tablesorterPager({container: $("#pager")});
 }
