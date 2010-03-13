cf508575db7d697157e01b766441de8f112717c8
Radu Ioan Fericean <rif@mac.net>
2010-03-13 22:58:51 +0200

    added mac for dev

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import socket
-if socket.gethostname() in ('sro-1503.sro.oce.net', 'grace', 'love', 'newcreation'):
+if socket.gethostname() in ('sro-1503.sro.oce.net', 'grace', 'love', 'newcreation', 'old-mac'):
     from settings_development import *
 else:
     from settings_production import *
