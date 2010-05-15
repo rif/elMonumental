f4ee8b43dfb45bc3d5d39456d548dab81dc253bb
Radu Ioan Fericean <radu@fericean.ro>
2010-05-15 18:06:36 +0300

    optimizat lista de participanti

--- scheduler/templatetags/scheduler_tags.py
+++ scheduler/templatetags/scheduler_tags.py
@@ -1,16 +1,10 @@
 from django import template
-from scheduler.models import MatchDay, Sport
-import logging
 
 register = template.Library()
 
 @register.tag
 def get_attendance(parser, token):
     return AttendanceNode()
-
-@register.tag
-def query_matchdays(parser, token):
-    return MatchdayQueryNode()
 
 @register.filter
 def attendance(value, arg):
@@ -20,31 +14,16 @@ def attendance(value, arg):
 def procentage(value, arg):
     return value[arg][1]
 
-class MatchdayQueryNode(template.Node):
-    def render(self, context):
-        for sport in context['sport_list']:
-            mds = MatchDay.objects.filter(sport_name=sport)
-            context[sport.name + '_query'] = mds
-            context[sport.name + '_counter'] = mds.count()
-        return ''
-
 """ Depends on MatchdayQueryNode being executed for efficiency """
 class AttendanceNode(template.Node):
     def render(self, context):
         username = context['profile'].user.username
         result_dict = {}
         for sport in context['sport_list']:
-            mds = context[sport.name + '_query']
-            mdsCount = context[sport.name + '_counter']
-            if context.has_key('since'):
-                mds = mds.filter(start_date__gte=context['since'])
-                mdsCount =  mds.count()
-            count = mds.filter(participants__username=username).count()
+            mdsCount = sport.matchday_sport__count
+            count = sport.matchday_sport.filter(participants__username=username).count()
             procentage = "NA"
-            if mdsCount > 0:
-                procentage = str(count*100/mdsCount) + "%"
-#            context[sport.name + '_count'] = str(count) + "/" + str(mdsCount)
-#            context[sport.name + '_procentage'] = procentage
+            if mdsCount > 0: procentage = str(count*100/mdsCount) + "%"
             result_dict[sport] = (str(count) + "/" + str(mdsCount), procentage)
         context['attendance_dict'] = result_dict
         return ''
